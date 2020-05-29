aaccfbc1be25a111a674737192b45fa504d8ce91
Erfan Abdi <erfangplus@gmail.com>
2020-05-29 11:53:26 +0430

    ubports: Build stub netd

--- sdm845.mk
+++ sdm845.mk
@@ -217,6 +217,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# Netd
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1-service.stub
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service.xiaomi_sdm845
