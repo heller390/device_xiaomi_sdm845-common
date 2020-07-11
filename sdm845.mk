c4805eb15dc43a64213a304c6fc91c1474a0e1d4
heller390 <indukurianvesh583@gmail.com>
2020-07-11 06:26:40 +0000

    Fix SAR

--- sdm845.mk
+++ sdm845.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vendor.mk)
